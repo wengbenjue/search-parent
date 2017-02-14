78769673bae3a1291a80ddb167488a49d77cfc0f
soledede.weng <wengbenjue@163.com>
2017-02-14 13:19:34 +0800

    spring boot web

--- search-core/src/main/java/search/Application.java
+++ search-core/src/main/java/search/Application.java
@@ -2,6 +2,7 @@ package search;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 
